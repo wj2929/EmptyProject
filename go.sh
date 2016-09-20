f8d6b55dd084245db1860baec1413b8a3234277d
wj2929 <wj2929@qq.com>
2016-09-20 16:50:24 +0800

    Update go.sh

--- go.sh
+++ go.sh
@@ -14,52 +14,42 @@ then
 	[ "$replacename" == "" ] && echo "替换项目名称不能为空！" && exit 1
 fi
 replace_files(){  
-    for file2 in `ls -a $1`  
-    do  
-        if [ x"$file2" != x"." -a x"$file2" != x".." -a x"$file2" != x".git" -a x"$file2" != x".gitignore" -a x"$file2" != x"lib" -a x"$file2" != x"bin" -a x"$file2" != x"obj" -a x"$file2" != x"packages" ];then  
-            	# echo "1:$1/$file2"
-            if [ -d "$1/$file2" ];then  
-                replace_files $1/$file2
-        #         if contains $1 $originalname
-        #     	then
-        #     		echo '3'
-		    		# #重命名文件夹名
-		    		# newdirpath=$(echo $1 | sed -r 's/'$originalname'/'$replacename'/g')
-		    		# mv $1 $newdirpath
-        #         fi
-	    	elif [ -s "$1/$file2" ];then  
+	for file2 in `ls -a $1`  
+	do  
+		if [ x"$file2" != x"." -a x"$file2" != x".." -a x"$file2" != x".git" -a x"$file2" != x".gitignore" -a x"$file2" != x"lib" -a x"$file2" != x"bin" -a x"$file2" != x"obj" -a x"$file2" != x"packages" ];then  
+			if [ -d "$1/$file2" ];then  
+			    replace_files $1/$file2
+			elif [ -s "$1/$file2" ];then  
 				if contains $file2 $originalname || contains $file2 ".sln" || contains $file2 ".csproj" || contains $file2 ".tt" || contains $file2 ".cs"
 				then  
-	                # echo '2'
-		    		# exit 0
-		    		#替换内容
-		    		sed -i 's/'$originalname'/'$replacename'/g' $1/$file2
-		    		#重命名文件名
-		    		newfilepath=$1/$(echo $file2 | sed -r 's/'$originalname'/'$replacename'/g')
-		    		[ "$1/$file2" != "$newfilepath" ] && mv $1/$file2 $newfilepath
-	    		fi
-            fi  
-        fi  
-    done  
+					#替换内容
+					sed -i 's/'$originalname'/'$replacename'/g' $1/$file2
+					#重命名文件名
+					newfilepath=$1/$(echo $file2 | sed -r 's/'$originalname'/'$replacename'/g')
+					[ "$1/$file2" != "$newfilepath" ] && mv $1/$file2 $newfilepath
+				fi
+			fi  
+		fi  
+	done  
 }  
 
 replace_dirs(){  
 	for file2 in `ls -a $1`  
-    do  
-        if [ x"$file2" != x"." -a x"$file2" != x".." -a x"$file2" != x".git" -a x"$file2" != x".gitignore" -a x"$file2" != x"lib" -a x"$file2" != x"bin" -a x"$file2" != x"obj" -a x"$file2" != x"packages" ];then
-    		if [ -d "$1/$file2" ];then  
-    			replace_dirs $1/$file2
+	do  
+		if [ x"$file2" != x"." -a x"$file2" != x".." -a x"$file2" != x.git" -a x"$file2" != x".gitignore" -a x"$file2" != x"lib" -a x"$file2" != x"bin" -a x"$file2" != x"obj" -a x"$file2" != x"packages" ];then
+			if [ -d "$1/$file2" ];then  
+				replace_dirs $1/$file2
 				# echo $1
 				if contains $1 $originalname
-            	then
-            		# echo '3'
-		    		#重命名文件夹名
-		    		newdirpath=$(echo $1 | sed -r 's/'$originalname'/'$replacename'/g')
-		    		mv $1 $newdirpath
-                fi    				
-    		fi
-    	fi  
-    done  
+				then
+					# echo '3'
+					#重命名文件夹名
+					newdirpath=$(echo $1 | sed -r 's/'$originalname'/'$replacename'/g')
+					mv $1 $newdirpath
+				fi    				
+			fi
+		fi  
+	done  
 }
 
 contains() {
@@ -77,4 +67,4 @@ contains() {
 replace_files ./
 # echo "replace_dirs"
 replace_dirs  ./
-echo "ok!"
+echo "ok!""
